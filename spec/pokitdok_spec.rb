347d8bc9aac74123ab091b0e4c64bdd1aedd8f9e
Denise Gosnell <denise.gosnell@pokitdok.com>
2017-02-09 17:24:47 -0500

    Added live eligibility tests for success and errors

--- spec/pokitdok_spec.rb
+++ spec/pokitdok_spec.rb
@@ -68,10 +68,79 @@ class PokitDokTest < MiniTest::Test
             trading_partner_id: 'MOCKPAYER'
         }
         response = @@pokitdok.eligibility @eligibility_query
-        assert response.keys == ["meta", "data"]
         refute_nil(response["meta"].keys)
         refute_nil(response["data"].keys)
         assert response["data"]["client_id"] == CLIENT_ID
+      end
+    end
+    describe 'Error Test: Missing Trading Partner ID' do
+      it 'make a call to eligibility without a Trading partner' do
+        @eligibility_query_2 = {
+            member: {
+                birth_date: '1970-01-01',
+                first_name: 'Jane',
+                last_name: 'Doe',
+                id: 'W000000000'
+            },
+            provider: {
+                first_name: 'JEROME',
+                last_name: 'AYA-AY',
+                npi: '1467560003'
+            },
+            service_types: ['health_benefit_plan_coverage'],
+        }
+        response = @@pokitdok.eligibility @eligibility_query_2
+        refute_nil(response["meta"].keys)
+        refute_nil(response["data"].keys)
+        assert response["data"]["errors"]["query"].to_s.include? "Unable to find configuration for trading_partner_id: None, transaction_set_name: eligibility"
+      end
+    end
+    describe 'Validation Error Test: Malformed Request' do
+      it 'make a call to eligibility with bad data' do
+        @bad_request = "bad request"
+        response = @@pokitdok.eligibility @bad_request
+        refute_nil(response["meta"].keys)
+        refute_nil(response["data"].keys)
+        assert response["data"]["errors"]["validation"].to_s.include? "This endpoint only accepts JSON requests of <type 'dict'>. Request provided was of <type 'unicode'>."
+      end
+    end
+    describe 'Validation Error Test: Malformed Request' do
+      it 'make a call to eligibility with bad data' do
+        @bad_request = {
+            member: {
+                birth_date: '1970-01-01',
+                first_name: 'Jane',
+                last_name: 'Doe',
+                id: '1'
+            },
+            trading_partner_id: 'MOCKPAYER'
+        }
+        response = @@pokitdok.eligibility @bad_request
+        refute_nil(response["meta"].keys)
+        refute_nil(response["data"].keys)
+        assert response["data"]["errors"]["validation"]["member"]["id"].to_s.include? "String value is too short."
+      end
+    end
+    describe 'Validation Error Test: Malformed Request' do
+      it 'make a call to eligibility with bad data' do
+        @bad_request = {
+            member: {
+                birth_date: '1970-01-01',
+                first_name: 'Jane',
+                last_name: 'Doe',
+                id: '100000000000'
+            },
+            provider: {
+                first_name: 'JEROME',
+                last_name: 'AYA-AY',
+                npi: '2'
+            },
+            trading_partner_id: 'MOCKPAYER'
+        }
+        response = @@pokitdok.eligibility @bad_request
+        refute_nil(response["meta"].keys)
+        refute_nil(response["data"].keys)
+        assert response["data"]["errors"]["validation"]["provider"]["npi"].to_s.include? "String value is too short."
       end
     end
   end
